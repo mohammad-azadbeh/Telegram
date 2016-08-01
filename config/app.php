7d69e6459153c56f2a48aa6625b12186a5dd4d3f
Mohammad-Azadbeh <Mohammad.Azadbeh@Gmail.Com>
2016-08-01 18:30:59 +0430

    Fix Bug

--- config/app.php
+++ config/app.php
@@ -147,7 +147,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-		Laraveldaily\Quickadmin\QuickadminServiceProvider::class,
+        Laraveldaily\Quickadmin\QuickadminServiceProvider::class,
+        Morilog\Jalali\JalaliServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -202,6 +203,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'jDate' => Morilog\Jalali\Facades\jDate::class,
 
     ],
 
